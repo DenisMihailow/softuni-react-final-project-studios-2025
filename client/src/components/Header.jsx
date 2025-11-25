e327fba798b61226f3f8eefd8718317ce5578a9f
DenisMihailow <barakata8@gmail.com>
2025-11-25 23:14:12 +0200

    First steps in my project
    Create Header and Home components

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router";
+export default function Header() {
+    return (
+         <header>
+        {/* <!-- Navigation --> */}
+        <nav>
+                <Link className="home" to="/"> 
+                    <img src="./images/logo.jpg" alt="logo" /> 
+                </Link>
+
+                <Link to="/games">Catalog</Link>
+
+                <div id="user">
+                    <Link to="/games/create">Add Game</Link>
+                    <Link to="/logout">Logout</Link>
+                </div>
+
+                <div id="guest">
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                </div>
+            </nav>
+    </header>
+    );
+}
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,35 @@
+// import "../styles/Home.css";
+//Nails Salon Services
+export default function Home() {
+    return (
+         <div className="nails">
+      <h1 className="title"></h1>
+
+      <div className="nails-cards">
+        <div className="nails-card">
+          <img src="./styles/nails1.jpg" alt="Manicure" />
+          <div className="nails-info">
+            <h3>Manicure</h3>
+            <button>Details</button>
+          </div>
+        </div>
+
+        <div className="nails-card">
+          <img src="./styles/nails2.jpg" alt="Gel Nails" />
+          <div className="nails-info">
+            <h3>Gel Nails</h3>
+            <button>Details</button>
+          </div>
+        </div>
+
+        <div className="nails-card">
+          <img src="./styles/nails3.jpg" alt="Nail Art" />
+          <div className="nails-info">
+            <h3>Nail Art</h3>
+            <button>Details</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    );
+}
