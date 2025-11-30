672e6aaa9231a848bb1a864c46cc43c8d68d546f
DenisMihailow <barakata8@gmail.com>
2025-11-30 11:10:50 +0200

    Logout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,25 @@ import { Route, Routes } from "react-router"
 import Header from "./components/Header"
 import Home from "./components/Home"
 import SaveYourTime from "./components/SaveYourTime"
+import Register from "./components/register/Register"
+import Login from "./components/login/Login"
+import  UserContext from "./contexts/UserContext"
+import { useContext } from "react"
 
 function App() {
 
+const {user} = useContext(UserContext);
+
   return (
     <>
-        <div className="global-background"></div>
-
-      <Header />
+     
+      <Header user={user}/>
 
       <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/nails/book" element={<SaveYourTime />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
       </Routes>
 
     </>
