292e4b46425697edc9598a750940964c2cc93755
DenisMihailow <barakata8@gmail.com>
2025-12-10 22:00:38 +0200

    fix catalog

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,4 +1,9 @@
+import { Link } from "react-router";
+import { useUserContext } from "../../contexts/UserContext";
+
 export default function Catalog() {
+
+    const {isAuthenticated} = useUserContext();
     return (
         <div className="catalog-wrapper">
 
@@ -6,21 +11,27 @@ export default function Catalog() {
         <img src="../images/nail.png" alt="" className="catalog-image" />
         <h3 className="catalog-title">Gel Nails</h3>
         <p className="catalog-text">Изискан маникюр за перфектен стил.</p>
-        <button className="catalog-btn">View More</button>
+         {isAuthenticated && (<Link to="/nails/book">
+          <button className="catalog-btn">View More</button>
+        </Link>)}
     </div>
 
     <div className="catalog-item">
         <img src="../images/pedicure.jpg" alt="" className="catalog-image" />
         <h3 className="catalog-title">Pedicure</h3>
         <p className="catalog-text">Грижа, комфорт и красота за твоите стъпала.</p>
-        <button className="catalog-btn">View More</button>
+        {isAuthenticated && (<Link to="/nails/book">
+          <button className="catalog-btn">View More</button>
+        </Link>)}
     </div>
 
     <div className="catalog-item">
         <img src="../images/terapy.jpg" alt="" className="catalog-image" />
         <h3 className="catalog-title">Терапия</h3>
         <p className="catalog-text">Релакс и възстановяване за тялото и ума.</p>
-        <button className="catalog-btn">View More</button>
+       {isAuthenticated && (<Link to="/nails/book">
+          <button className="catalog-btn">View More</button>
+        </Link>)}
     </div>
 
 </div>
